5078051fb070a017e96d06674aa3533893c3ab86
nicholusbriz <atbriz256@gmail.com>
2026-03-31 15:31:07 +0300

    update

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    sameSite: 'lax',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000
   }
